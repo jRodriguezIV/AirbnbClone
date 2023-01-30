0533263e74e106f91672885279fa83dbf211c370
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-01-30 13:31:57 -0500

    added LargeCard and Footer components

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function SmallCard({img, location, distance}) {
   return (
-    <div className=' text-black flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
+    <div className=' text-black flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out '>
         {/* left */}
         <div className='relative h-16 w-16'>
             <Image src={img} layout='fill' className='rounded-lg' />
@@ -12,7 +12,7 @@ function SmallCard({img, location, distance}) {
         {/* right */}
         <div>
             <h2>{location}</h2>
-            <h3>{distance}</h3>
+            <h3 className='text-gray-500'>{distance}</h3>
         </div>
 
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,14 @@ import Banner from 'components/Banner'
 import { Inter } from '@next/font/google'
 import styles from '<clone>/styles/Home.module.css'
 import SmallCard from 'components/SmallCard'
+import MediumCard from 'components/MediumCard'
+import LargeCard from 'components/LargeCard'
+import Footer from 'components/Footer'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <>
       <Head>
@@ -29,12 +33,33 @@ export default function Home({ exploreData }) {
             <h2 className=' text-black text-4xl font-semibold pb-5 '>Explore Nearby</h2>
 
             {/* Pull some data from a server - API endpoints */}
-            {exploreData?.map(({img, distance, location}) => (
-              <SmallCard key={img} img={img} distance={distance} location={location} />
-            ))}
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+              {exploreData?.map(({ img, distance, location }) => (
+                <SmallCard key={img} img={img} distance={distance} location={location} />
+              ))}
+            </div>
 
           </section>
+
+          <section>
+             <h2 className=' text-black text-4xl font-semibold py-8'>Live Anywhere</h2>
+            <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {cardsData?.map(item => (
+              <MediumCard key={item.img} img={item.img} title={item.title}/>
+            ))} 
+            </div>
+          </section>
+
+          <LargeCard
+          img='https://links.papareact.com/4cj'
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb."
+          buttonText="Get Inspired"
+          />
+
         </main>
+
+        <Footer/>
 
       </div>
     </>
@@ -44,10 +69,13 @@ export default function Home({ exploreData }) {
 
 export async function getStaticProps() {
   const exploreData = await fetch('https://www.jsonkeeper.com/b/4G1G').then((res) => res.json());
-  
+
+  const cardsData = await fetch('https://www.jsonkeeper.com/b/VHHT').then((res) => res.json())
+
   return {
     props: {
-      exploreData : exploreData
+      exploreData: exploreData,
+      cardsData: cardsData
     }
   };
 }
